6e26232fccb03024a3e352fb5d6a4699fcaa9514
agomezomg <agomez98@unitec.edu>
2017-09-11 15:03:09 -0600

    smth

--- ProyectoTBD1/src/Asesor.java
+++ ProyectoTBD1/src/Asesor.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,9 +12,12 @@
  * @author lana
  */
 public class Asesor extends AsesorMecanico {
-
+    
+    private final ArrayList<Mecanico> assignedGroup;
+    
     public Asesor(String amName, int amID, int amCompanyPhone) {
         super(amName, amID, amCompanyPhone);
+        assignedGroup = new ArrayList<>();
     }
 
 }
--- ProyectoTBD1/src/Mecanico.java
+++ ProyectoTBD1/src/Mecanico.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,9 +12,11 @@
  * @author lana
  */
 public class Mecanico extends AsesorMecanico {
+    private final ArrayList<Citas> registeredDates;
     
     public Mecanico(String amName, int amID, int amCompanyPhone) {
         super(amName, amID, amCompanyPhone);
+        registeredDates = new ArrayList<>();
     }
 
 }
