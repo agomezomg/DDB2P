eeea8d3404ea8e81a2af43b8a6eea7d158d1ea1f
agomezomg <agomez98@unitec.edu>
2017-09-23 21:31:40 -0600

    :)

--- ProyectoTBD1/src/Conectar.java
+++ ProyectoTBD1/src/Conectar.java
@@ -1,19 +1,33 @@
-import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
-import java.sql.*;
 
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import java.awt.HeadlessException;
+import java.sql.*;
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Conectar {
-    Connection conectar=null;
-    public Connection Conexion(){
-        try{
-            MysqlDataSource ds = new MysqlDataSource();
-            ds.setServerName("127.0.0.1");
-            //ds.setPort();//No Need of port for local servers
-            ds.setDatabaseName("proyecto");
-            ds.setUser("root");
-            ds.setPassword("");
-            conectar = ds.getConnection();
-        }catch(Exception e){
+
+    Connection conectar = null;
+
+    public Connection Conexion() {
+        try {
+            try {
+                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3305/phpmyadmin", "root", "");
+                JOptionPane.showMessageDialog(null, "connected with " + con.toString());
+                MysqlDataSource ds = new MysqlDataSource();
+                ds.setServerName("127.0.0.1");
+                //ds.setPort();//No Need of port for local servers
+                ds.setDatabaseName("proyecto");
+                ds.setUser("root");
+                Scanner sc = new Scanner(System.in);
+                System.out.println("PASSWORD PROMPT: ");
+                String pass = sc.nextLine();
+                ds.setPassword(pass);
+                conectar = con;
+            } catch (SQLException | HeadlessException e) {
+                JOptionPane.showMessageDialog(null, "not connect to server and message is: " + e.getMessage());
+            }
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         return conectar;
